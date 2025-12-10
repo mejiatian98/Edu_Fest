9c490fdf654caa4e24aff9ae7fd3558e2ccb9533
mejiatian98 <halosniper04@gmail.com>
2025-12-09 20:08:17 -0500

    Proyecto 7

--- principal_eventos/settings.py
+++ principal_eventos/settings.py
@@ -134,13 +134,20 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-    BASE_DIR / "app_admin_eventos/static",
-    BASE_DIR / "app_participantes/static",
-    BASE_DIR / "app_asistentes/static",
-]
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+
+
+# STATIC_URL = 'static/'
+# STATICFILES_DIRS = [
+#     BASE_DIR / "static",
+#     BASE_DIR / "app_admin_eventos/static",
+#     BASE_DIR / "app_participantes/static",
+#     BASE_DIR / "app_asistentes/static",
+# ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
