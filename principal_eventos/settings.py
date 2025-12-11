179507bb01ea8ca0518b81e340966258b5818e87
mejiatian98 <halosniper04@gmail.com>
2025-12-10 21:34:50 -0500

    Proyecto 27

--- principal_eventos/settings.py
+++ principal_eventos/settings.py
@@ -151,9 +151,6 @@ if IS_PRODUCTION:
 
     AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
 
-    AWS_DEFAULT_ACL = None
-    AWS_QUERYSTRING_AUTH = False
-    AWS_S3_FILE_OVERWRITE = False
 
     DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 
