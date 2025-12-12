e34db00c01d06ab7feb6bb2ae15203f187cf32d3
mejiatian98 <halosniper04@gmail.com>
2025-12-11 19:11:05 -0500

    Proyecto 36

--- principal_eventos/settings.py
+++ principal_eventos/settings.py
@@ -154,11 +154,11 @@ if IS_PRODUCTION:
     AWS_STORAGE_BUCKET_NAME = os.getenv("AWS_STORAGE_BUCKET_NAME")
     AWS_S3_REGION_NAME = os.getenv("AWS_S3_REGION_NAME")
 
-    AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
+    AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.{AWS_S3_REGION_NAME}.s3.amazonaws.com"
 
     AWS_QUERYSTRING_AUTH = False
     AWS_S3_FILE_OVERWRITE = False
-    AWS_DEFAULT_ACL = "bucket-owner-full-control"
+    AWS_DEFAULT_ACL = None  
 
 
     DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
